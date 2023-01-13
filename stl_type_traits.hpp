ed004e6ba65c33c75f0914927525a47e1db73f92
caio-vinicius <contato@caiovinicius.dev.br>
2023-01-13 20:33:14 -0300

    implementation of is_integral in type traits

--- stl_type_traits.hpp
+++ stl_type_traits.hpp
@@ -11,8 +11,31 @@ namespace ft {
   template <typename T>
   struct enable_if<true, T> {typedef T type;};
 
+  template <typename T, T v>
+  struct integral_constant {
+    static const T value = v;
+    typedef T value_type;
+    typedef integral_constant<T, v> type;
+    operator T() {return (v);};
+  };
+
+  typedef integral_constant<bool, true> true_type;
+  typedef integral_constant<bool, false> false_type;
+
   template<typename T>
-  struct is_integral;
+  struct is_integral : false_type {};
+
+  template<> struct is_integral<bool> : true_type {};
+  template<> struct is_integral<char> : true_type {};
+  template<> struct is_integral<wchar_t> : true_type {};
+  template<> struct is_integral<signed char> : true_type {};
+  template<> struct is_integral<short int> : true_type {};
+  template<> struct is_integral<int> : true_type {};
+  template<> struct is_integral<long int> : true_type {};
+  template<> struct is_integral<unsigned char> : true_type {};
+  template<> struct is_integral<unsigned short int> : true_type {};
+  template<> struct is_integral<unsigned int> : true_type {};
+  template<> struct is_integral<unsigned long int> : true_type {};
 }
 
 #endif
